4061bd77e5a069446cfd27794a5274e6ed1fc7b6
OmerCanOzdemirWeDoIT <omer.can.ozdemir@wedoit.be>
2022-05-06 15:21:47 +0200

    feat: dotnet test api

---
+++ hello-world/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hello_world.Controllers
+{
+    [Controller]
+    [Route("api/[action]")]
+    public class HelloController : ControllerBase
+    {
+
+        public ActionResult Hello()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
